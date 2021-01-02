48ef31a3119ab34df558eb566cfd3cefc4ab08d2
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-01-02 04:58:22 +0000

    🚀 add friendly-words package to gen team names

--- api/westworld/register.js
+++ api/westworld/register.js
@@ -1,4 +1,5 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+const friendlyWords = require('friendly-words');
 const AirtablePlus = require('airtable-plus');
 const airtable = new AirtablePlus({
   baseID: 'appHsDh9IfVlYNqU1',
@@ -9,15 +10,16 @@ const airtable = new AirtablePlus({
 module.exports = async (req, res) => {
   if (req.query.auth) {
     const groupId = nanoid(16);
+    const groupname = friendlyWords.teams[Math.floor(Math.random() * friendlyWords.teams.length) + 1] + '-' + friendlyWords.objects[Math.floor(Math.random() * friendlyWords.objects.length) + 1];
     let aiRes = await airtable.create({
       id: groupId,
-      name: req.query.name || 'No name specified',
+      name: req.query.name ?? groupname,
       completion: 0,
       lastcompletedchallenge: 0,
     });
     res.json({
       status: 200,
-      message: `Group ${groupId} with ${req.query.name ? `name ${req.query.name}` : 'no name'} created!`,
+      message: `Group ${groupId} with name ${req.query.name ?? groupname} created!`,
       record: aiRes.id,
       id: groupId,
     });
